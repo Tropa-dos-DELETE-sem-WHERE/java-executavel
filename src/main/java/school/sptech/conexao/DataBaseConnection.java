77dd95f520654a82695b5d0f05ead0fd4b7aaaa3
Dennis <denniswilson17@hotmail.com>
2025-12-11 08:31:09 -0300

    mudança de ip ec2

--- src/main/java/school/sptech/conexao/DataBaseConnection.java
+++ src/main/java/school/sptech/conexao/DataBaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
-    private static final String url = "jdbc:mysql://54.173.243.6:3306/educadata";
+    private static final String url = "jdbc:mysql://54.221.243.95:3306/educadata";
     private static final String usuario = "Caramico";
     private static final String senha = "urubu100";
 
